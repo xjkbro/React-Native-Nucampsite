c40e4ca9b98b7214ecb47f46b8f1c9a4c0673885
Jason-Kyle De Lara <jason8oh8@gmail.com>
2021-01-06 18:09:52 -0800

    Redux Adding Favorites

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -6,6 +6,7 @@ import { PROMOTIONS } from "../shared/promotions";
 import { PARTNERS } from "../shared/partners";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = (state) => {
     return {
@@ -15,7 +16,19 @@ const mapStateToProps = (state) => {
     };
 };
 
-function RenderItem({ item }) {
+function RenderItem(props) {
+    const { item } = props;
+
+    if (props.isLoading) {
+        return <Loading />;
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
     if (item) {
         return (
             <Card
@@ -43,6 +56,8 @@ class Home extends Component {
                             (campsite) => campsite.featured
                         )[0]
                     }
+                    isLoading={this.props.campsites.isLoading}
+                    errMess={this.props.campsites.errMess}
                 />
                 <RenderItem
                     item={
@@ -50,6 +65,8 @@ class Home extends Component {
                             (promotion) => promotion.featured
                         )[0]
                     }
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess}
                 />
                 <RenderItem
                     item={
@@ -57,6 +74,8 @@ class Home extends Component {
                             (partner) => partner.featured
                         )[0]
                     }
+                    isLoading={this.props.partners.isLoading}
+                    errMess={this.props.partners.errMess}
                 />
             </ScrollView>
         );
